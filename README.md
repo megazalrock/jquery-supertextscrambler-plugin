a7328f1edcac7e95c8b4c8b9b0e5326a8b8cb0f5
otto <otto@mgzl.jp>
2014-03-08 01:22:45 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# jQuery Super Text Scrambler
+
+The best jQuery plugin ever.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/otto/jquery-supertextscrambler-plugin/master/dist/supertextscrambler.min.js
+[max]: https://raw.github.com/otto/jquery-supertextscrambler-plugin/master/dist/supertextscrambler.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/supertextscrambler.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/jquery-supertextscrambler-plugin.js
@@ -0,0 +1,91 @@
+/*! jQuery Super Text Scrambler 2014-03-08
+ *  Vertion : 0.1.0
+ *  Dependencies : jQuery *
+ *  Author : MegazalRock (Otto Kamiya)
+ *  Copyright (c) 2014 MegazalRock (Otto Kamiya);
+ *  License : */
+(function(window, $, undefined) {
+	var SuperTextScrambler = function(text, options){
+		options = options || {};
+		this.options = $.extend(true, {
+			mode: 'en',
+			wait: 1000,
+			interval: 60
+		}, options);
+		this.text = text;
+		this.length = text.length;
+		this.currentText = 0;
+	};
+
+	SuperTextScrambler.prototype.init = function($target){
+		var sts = this;
+		sts.$target = $target;
+		$target
+			.html('&nbsp;')
+			.trigger('scramblerStart');
+	};
+
+	SuperTextScrambler.prototype.start = function(){
+		var sts = this, interval = sts.options.interval;
+		setTimeout(function(){
+			(function loop(){
+				if(sts.currentText < sts.length){
+					sts.step();
+					setTimeout(loop, interval);
+				}else{
+					sts.end();
+				}
+			})();
+		}, sts.options.wait);
+	};
+
+	SuperTextScrambler.prototype.step = function(){
+		var sts = this;
+		sts.$target
+			.text(sts.text.slice(0, sts.currentText + 1) + sts.getRndText(sts.length - sts.currentText - 1, sts.options.mode));
+
+		sts.currentText += 1;
+	};
+
+	SuperTextScrambler.prototype.getRndText = function(length, mode){
+		mode = mode || 'en';
+
+		var startCharCode, endCharCode , rndText = [], i = 0;
+
+		if(mode === 'en'){
+			startCharCode = 65;
+			endCharCode = 122;
+		}else{
+			startCharCode = 0x4E00;
+			endCharCode = 0x9FA0;
+		}
+		for(;i < length; i ++){
+			rndText.push(String.fromCharCode(startCharCode + Math.floor(Math.random() * (endCharCode - startCharCode))));
+		}
+
+		return rndText.join('');
+
+	};
+
+	SuperTextScrambler.prototype.end = function(){
+		var sts = this;
+		sts.$target
+			.trigger('scramblerEnd');
+	};
+
+	$.extend({
+		'SuperTextScrambler': function (text, options){
+			return new SuperTextScrambler(text, options);
+		}
+	});
+	$.fn.extend({
+		'superTextScrambler':function (options){
+			var sts = new SuperTextScrambler($(this).text(), options);
+			sts.init($(this));
+			sts.start();
+			$(this).data('SuperTextScrambler', sts);
+			return this;
+		}
+	});
+
+}(this, jQuery));
