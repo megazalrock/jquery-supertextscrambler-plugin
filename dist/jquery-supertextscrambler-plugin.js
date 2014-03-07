fa36f5007ce057e6520d3ddbc50ab917dd0b8277
otto <otto@mgzl.jp>
2014-03-08 02:17:07 +0900

    会主

--- dist/jquery-supertextscrambler-plugin.js
+++ dist/jquery-supertextscrambler-plugin.js
@@ -5,16 +5,44 @@
  *  Copyright (c) 2014 MegazalRock (Otto Kamiya);
  *  License : */
 (function(window, $, undefined) {
+
+	(window.requestAnimationFrame =
+		window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame    ||
+		window.oRequestAnimationFrame      ||
+		window.msRequestAnimationFrame     ||
+		function(callback) {
+			return window.setTimeout(function() {
+				callback(Date.now());
+			}, 1000 / 60);
+		}
+	);
+
+	(window.cancelAnimationFrame =
+		window.cancelAnimationFrame ||
+		window.webkitCancelAnimationFrame ||
+		window.mozCancelAnimationFrame ||
+		window.oCancelAnimationFrame ||
+		window.msCancelAnimationFrame ||
+		function(id){
+			return clearTimeout(id);
+		}
+	);
+
+
 	var SuperTextScrambler = function(text, options){
 		options = options || {};
 		this.options = $.extend(true, {
 			mode: 'en',
 			wait: 1000,
-			interval: 60
+			fps: 30,
+			textFps: 30
 		}, options);
 		this.text = text;
 		this.length = text.length;
-		this.currentText = 0;
+		this.currentTextLength = 0;
+		this.currentText = '';
 	};
 
 	SuperTextScrambler.prototype.init = function($target){
@@ -26,25 +54,52 @@
 	};
 
 	SuperTextScrambler.prototype.start = function(){
-		var sts = this, interval = sts.options.interval;
+		var sts = this, $target = sts.$target, length = sts.length, mode = sts.options.mode;
+		var delta, now, then = Date.now(), interval = 1000 / sts.options.fps;
+		var textDelta, textThen = then, textInterval = 1000 / sts.options.textFps;
+		var num = Math.floor(sts.options.textFps / 30) + (sts.options.textFps % 30 ? 1 : 0);
+		
 		setTimeout(function(){
-			(function loop(){
-				if(sts.currentText < sts.length){
-					sts.step();
-					setTimeout(loop, interval);
+			(function frame(){
+				now = Date.now();
+				sts.rafId = window.requestAnimationFrame(frame);
+				delta = now - then;
+				textDelta = now - textThen;
+				if(sts.currentTextLength < length){
+					if(delta >= interval){
+						if(textDelta >= textInterval){
+							$target
+								.text(sts.stepText(num) + sts.getRndText(length - sts.currentTextLength - 1, mode));
+						}else{
+							$target
+								.text(sts.currentText + sts.getRndText(length - sts.currentTextLength - 1, mode));
+						}
+						then = now - (delta % interval);
+						textThen = now - (textDelta % textInterval);
+					}
 				}else{
+					window.cancelAnimationFrame(sts.rafId);
 					sts.end();
 				}
 			})();
 		}, sts.options.wait);
 	};
 
+	SuperTextScrambler.prototype.stepText = function(num){
+		var sts = this, i = 0;
+		for(; i < num; i += 1){
+			sts.currentText = sts.text.slice(0, sts.currentTextLength + 1);
+			sts.currentTextLength = sts.currentText.length;
+		}
+		return sts.currentText;
+	};
+
 	SuperTextScrambler.prototype.step = function(){
 		var sts = this;
 		sts.$target
-			.text(sts.text.slice(0, sts.currentText + 1) + sts.getRndText(sts.length - sts.currentText - 1, sts.options.mode));
+			.text(sts.text.slice(0, sts.currentTextLength + 1) + sts.getRndText(sts.length - sts.currentTextLength - 1, sts.options.mode));
 
-		sts.currentText += 1;
+		sts.currentTextLength += 1;
 	};
 
 	SuperTextScrambler.prototype.getRndText = function(length, mode){
@@ -80,10 +135,13 @@
 	});
 	$.fn.extend({
 		'superTextScrambler':function (options){
-			var sts = new SuperTextScrambler($(this).text(), options);
-			sts.init($(this));
-			sts.start();
-			$(this).data('SuperTextScrambler', sts);
+			$(this)
+				.each(function(){
+					var sts = new SuperTextScrambler($(this).text(), options);
+					sts.init($(this));
+					sts.start();
+					$(this).data('SuperTextScrambler', sts);
+				});
 			return this;
 		}
 	});
